50c3f5aa37538742e30b781554ad357c66a3be5f
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-01-20 19:54:34 +0300

    labi23

---
+++ labs/lab3/script.js
@@ -0,0 +1,29 @@
+window.onload = () =>{
+    let body = document.querySelector("body");
+    let block = document.querySelector('.block');
+    let min2 = document.getElementById("min2")
+    let min3 = document.getElementById("min3")
+
+
+    block.ondblclick = () =>{
+        block.style.backgroundColor ="blue"; 
+    }
+
+    body.onclick = (event)=>{
+        if (event.target.className == 'block'){
+            block.style.backgroundColor ="red"; 
+        }
+    };
+    
+    function YaRotEbalEtoyFunkcii(El){
+        El.style.backgroundColor ="red"; 
+    }
+
+    min2.addEventListener('click', () =>{
+        min2.style.backgroundColor ="red"; 
+    })
+
+    min3.onclick = ()=>{
+        min3.style.backgroundColor ="red";
+    }
+}
