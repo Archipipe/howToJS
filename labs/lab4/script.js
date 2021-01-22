969a5f0abbc6c2798cf98c4c8525090d995755fa
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-01-22 12:42:36 +0300

    sasi

--- labs/lab4/script.js
+++ labs/lab4/script.js
@@ -3,21 +3,46 @@ window.onload = () => {
     let body = document.querySelector("body")
 
     let sdvig = 0;
+    let topec = 0;
+    let num = 1;
     function move(){
-        if (sdvig / 1920 == 1){
-            sdvig += 40;
-            block.style.left = sdvig+'px';
-            block.style.top = Math.pow(sdvig, 0.75) - 15 + "px";
+        if(sdvig == 1820){
+            num *=-1
+        }
+        if(sdvig == -1){
+            num *=-1
+        }
+        if (num == 1) {
+            block.style.top = topec + 'px';
+            block.style.left = sdvig + 'px';
+
+            topec = Math.pow(sdvig, 0.75) - 15;
+            sdvig += 1;
+
             console.log(sdvig)
-            setTimeout(move,250);
+            setTimeout(move,1);
+
+            let tochka = document.createElement('div')
+            tochka.classList.add("tochka")
+            tochka.style.left = 95 + sdvig + 'px';
+            tochka.style.top = 15 + topec + 'px';
+            body.appendChild(tochka)
+        }
+
+        if (num == -1) {
+            block.style.top = topec + 'px';
+            block.style.left = sdvig + 'px';
+
+            topec = Math.pow(sdvig, 0.75) - 15;
+            sdvig -= 1;
+            
+
+            console.log(sdvig)
+            setTimeout(move,1);
         }
     }
 
-    block.onclick = (e) =>{
-        block.style.left = "20px";
-        block.style.top = "330px";
-
+    block.onmouseout = (e) =>{
         move();
-
     }
 }
