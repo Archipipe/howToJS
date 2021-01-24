7339e32737f65b4ab097ab6f1b8d397cbdbee606
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-01-24 22:25:07 +0300

    popravki

--- labs/lab5/script.js
+++ labs/lab5/script.js
@@ -1,48 +1,65 @@
 window.onload = () =>{
     let a1 = -1
-    let a2 = -1
-    let a3 = -1
     let a = []
     sloy1 = document.querySelector('.sloy1')
     sloy2 = document.querySelector('.sloy2')
     sloy3 = document.querySelector('.sloy3')
     help = document.querySelector(".help")
-    text = document.querySelector(".text")
+    comm = document.querySelector(".comm")
     body = document.querySelector('body')
 
     document.onkeydown = (event) =>{
         a.push(event.code)
         if(a[0] == "KeyW" && a[1] == "Digit1"){
-            a1 *= -1
-            if (a1 == 1){
-                sloy1.style.display = 'none'
-                a.length = 0
-            }
+            sloy1.style.zIndex = '3'
+            sloy2.style.zIndex = '2'
+            sloy3.style.zIndex = '1'
+            a.length = 0
             
         }
 
         if(a[0] == "KeyW" && a[1] == "Digit2"){
-            a2 *= -1
-            if (a1 == 1){
-                sloy2.style.display = 'none'
-                a.length = 0
-            }
+            sloy1.style.zIndex = '2'
+            sloy2.style.zIndex = '3'
+            sloy3.style.zIndex = '1'
+            a.length = 0
             
         }
 
         if(a[0] == "KeyW" && a[1] == "Digit3"){
-            a3 *= -1
-            if (a1 == 1){
-                sloy3.style.display = 'none'
-                a.length = 0
-            }
+            sloy1.style.zIndex = '1'
+            sloy2.style.zIndex = '2'
+            sloy3.style.zIndex = '3'
+            a.length = 0
             
         }
 
-        if (event.code =="KeyP"){
+        if (event.shiftKey){
+            comm.innerHTML = "Комментарий <br><br><br> Ты когда-нибудь нахуй видел, как выглядят зубы пингвина? Загугли, это пиздец."
+            a.length = 0
+        }
+
+        if (event.ctrlKey){
+            comm.innerHTML = "Комментарий"
+            a.length = 0
+        }
+
+        if(event.code == "F1"){
+            event.preventDefault();
+            help.innerHTML = "Короче, если нажмёшь на Shift, ты увидишь комментарий. <br> При комбинации клавиш W+1,2,3 - ты будешь менять местами карточки с Моршу. <br> Там ещё надо, конечно, высвечивать две карточки одновременно, но я не понял, да и похуй, я спать. "
+            a.length = 0
+        }
+
+        if (a.length == 3){
+            a.length = 0
+        }
+
+        if (event.code == "KeyZ"){
             console.log(a)
             a.length = 0
         }
+
+        
     }
 
 }
