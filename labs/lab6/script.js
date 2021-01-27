dd292994cc813fc74784c45a6915eab5ffbc0cae
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-01-27 15:54:39 +0300

    x

--- labs/lab6/script.js
+++ labs/lab6/script.js
@@ -1,7 +1,7 @@
 window.onload = () =>{
     var time = performance.now();
-    let a = -20000
-    let y = Math.log((12*2)+Math.sqrt(12))+Math.LN2
+    let x = 12
+    let y = Math.log((x*2)+Math.sqrt(x))+Math.LN2
     time = performance.now() - time;
     console.log('Время выполнения = ', time)+ 'микросекунд';
 }
