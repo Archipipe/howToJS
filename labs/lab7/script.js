0c2126f154d60e7a85f1033e9b054241fd992907
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-02-08 23:38:29 +0300

    Ebobnie vahi pechen'e

--- labs/lab7/script.js
+++ labs/lab7/script.js
@@ -1,3 +1,43 @@
 window.onload = () =>{
-    
+    let vvod = document.querySelector("#newUser");
+    let getUser = document.querySelector("#getUser");
+    let output = document.querySelector("#output")
+    let result = document.querySelector("#outputText")
+    let a = 0
+
+    let user = {
+        "male" : "",
+        "education" : ""
+    }
+
+    let file ={}
+
+    vvod.onclick = () =>{
+
+        getUser.onclick = () =>{
+            let selectedGen = document.querySelector('input[name="option1"]:checked')
+            user["male"] = selectedGen.value
+
+            let selectedEd = document.querySelector('input[name="option2"]:checked')
+            user["education"] = selectedEd.value
+
+            a+= 1 
+
+            if (a!=6){
+                file[a] = user
+            } else {
+                alert("Слишкам дохуя юзеров иди нахуй крч")
+            }
+            
+
+            let JSONFile = JSON.stringify(file)
+            document.cookie = JSONFile
+
+        }
+
+    }
+
+    output.onclick
+
+
 }
