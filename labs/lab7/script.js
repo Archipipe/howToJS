bbd724956d01f4e8aa3afe443d623db763679df3
Archipipe <77447627+Archipipe@users.noreply.github.com>
2021-02-09 20:16:39 +0300

    ЭТА ХУЙНЯ НЕ РАБОТАЕТ БЛЯТЬ Я РОТ ЕБАЛ

--- labs/lab7/script.js
+++ labs/lab7/script.js
@@ -13,6 +13,7 @@ window.onload = () =>{
     let file ={}
 
     vvod.onclick = () =>{
+        console.log(users)
 
         getUser.onclick = () =>{
             let selectedGen = document.querySelector('input[name="option1"]:checked')
@@ -28,16 +29,47 @@ window.onload = () =>{
             } else {
                 alert("Слишкам дохуя юзеров иди нахуй крч")
             }
+
+
+            user = {}
             
-
-            let JSONFile = JSON.stringify(file)
-            document.cookie = JSONFile
-
+            document.cookie = JSON.stringify(file)
+            
         }
 
     }
 
-    output.onclick
+    output.onclick = () => {
+        let users = JSON.parse(document.cookie)
+        let avrMen = 0
+        let avrWom = 0
+        let avrPrim = 0
+        let avrMid = 0
+        let avrHigh = 0
+        for (let i = 1; i==a;i++){
+            if (users[`${i}`]["male"] == 1 ){
+                avrMen++
+            } else {
+                avrWom++
+            }
+
+            if (users[`${i}`]["education"] == 1){
+                avrPrim++
+            } else if (users[`${i}`]["education"] == 2){
+                avrMid++
+            } else {
+                avrHigh++
+            }
+        }
+
+        avrMen = (avrMen * 100) / a
+        avrWom = (avrWom * 100) / a
+        avrPrim = (avrPrim * 100) / a
+        avrMid = (avrMid * 100) / a
+        avrHigh = (avrHigh * 100) / a
+        let result = document.querySelector("#outputText")
+        result.innerHTML = `1. % женщин : ${avrWom}% <br> 2. % мужчин : ${avrMen}% <br> 3. % высш. обр. : ${avrHigh}% <br> 4. % сред. обр. : ${avrMid}% <br> 5. % нач. обр. : ${avrPrim}%`
+    }
 
 
 }
